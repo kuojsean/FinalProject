f9da639f3eb9dfb8d656540f424049e310dfb45e
kuojsean <kuo.j.sean@gmail.com>
2018-05-29 09:29:47 -0600

    created movements with key listener.

--- src/snake/model/Screen.java
+++ src/snake/model/Screen.java
@@ -2,6 +2,8 @@ package snake.model;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.util.*;
 
 
@@ -26,10 +28,17 @@ public class Screen extends JPanel implements Runnable
 	private boolean down = false;
 	
 	private int ticks = 0;
+	private int tickDifficulty = 600000;
+	
+	private Key key;
 	
 	public Screen()
 	{
+		setFocusable(true);
+		key = new Key();
+		addKeyListener(key);
 		setPreferredSize( new Dimension(width,height));
+		
 		
 		snake = new ArrayList<SnakePart>();
 		
@@ -45,7 +54,7 @@ public class Screen extends JPanel implements Runnable
 		}
 		
 		ticks++;
-		if (ticks > 250000)
+		if (ticks > tickDifficulty)
 		{
 			if(right)
 			{
@@ -57,17 +66,29 @@ public class Screen extends JPanel implements Runnable
 			}
 			if(up)
 			{
-				y++;
+				y--;
 			}
 			if(down)
 			{
-				y--;
+				y++;
+			}
+			
+			ticks = 0;
+			
+			body = new SnakePart(x,y, 10);
+			snake.add(body);
+			
+			if(snake.size() > size)
+			{
+				snake.remove(0);
 			}
 		}
+
 	}
 	
 	public void paint(Graphics graphics)
 	{
+		graphics.clearRect(0, 0, width, height);
 		for(int index = 0; index < snake.size(); index++)
 		{
 			snake.get(index).draw(graphics);
@@ -93,6 +114,62 @@ public class Screen extends JPanel implements Runnable
 		{
 			tick();
 			repaint();
+		}
+	}
+	
+	private class Key implements KeyListener
+	{
+
+		@Override
+		public void keyTyped(KeyEvent e)
+		{
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void keyPressed(KeyEvent e)
+		{
+			int key = e.getKeyCode();
+			
+			if (key == KeyEvent.VK_RIGHT && !left)
+			{
+				up = false;
+				down = false;
+				right = true;
+				left = false;
+			}
+			if (key == KeyEvent.VK_LEFT && !right)
+			{
+				up = false;
+				down = false;
+				left = true;
+				right = false;
+			}
+			
+			if (key == KeyEvent.VK_UP && !down)
+			{
+				up = true;
+				down = false;
+				left = false;
+				right = false;
+			}
+			
+			if (key == KeyEvent.VK_DOWN && !up)
+			{
+				up = false;
+				down = true;
+				left = false;
+				right = false;
+			}
+			
+		}
+
+		@Override
+		public void keyReleased(KeyEvent e)
+		{
+			// TODO Auto-generated method stub
+			
 		}
 	}
 }
